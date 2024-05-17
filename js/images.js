370fc58f8609fb5af9af1122ce7f018002ec5a21
Neil Hawkins <neilh@cogapp.com>
2024-05-17 16:29:28 +0100

    v1

--- js/images.js
+++ js/images.js
@@ -1,31 +1,18 @@
-/* script.js */
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector(".visualizer");
-  const imageSources = [
-    'https://www.artic.edu/iiif/2/e8e67721-bbb1-d007-82bd-c430ea73db70/full/843,/0/default.jpg',
-  ]; // Add more image URLs if needed
+function addImage(image) {
+  const container = document.querySelector('.visualizer');
+  const img = document.createElement('img');
+  img.src = `https://www.artic.edu/iiif/2/${image.image_id}/full/!600,600/0/default.jpg`;
+  img.className = 'throbbing';
 
-  function addImage(src) {
-    const img = document.createElement('img');
-    img.src = src;
-    img.className = 'throbbing';
+  // Random position within the container
+  const containerRect = container.getBoundingClientRect();
+  const maxX = containerRect.width - 600;
+  const maxY = containerRect.height - 400;
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
 
-    // Random position within the container
-    const containerRect = container.getBoundingClientRect();
-    const maxX = containerRect.width - 600;
-    const maxY = containerRect.height - 400;
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
+  img.style.left = `${randomX}px`;
+  img.style.top = `${randomY}px`;
 
-    img.style.left = `${randomX}px`;
-    img.style.top = `${randomY}px`;
-
-    container.appendChild(img);
-  }
-
-  // Add a new image every second
-  setInterval(() => {
-    const randomImage = imageSources[Math.floor(Math.random() * imageSources.length)];
-    addImage(randomImage);
-  }, 2000);
-});
+  container.appendChild(img);
+}
